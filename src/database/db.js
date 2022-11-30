c4398b034e64c68308449ee68a7fc8ea5a95f576
Pedro Reis <phbarcelosreis66@gmail.com>
2022-11-30 15:45:21 -0300

    feat: add archives with basic commands and start of develop

---
+++ src/database/db.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+try {
+    await mongoClient.connect();
+} catch (err) {
+    console.log(err);
+}
+
+const db = mongoClient.db("Drivencracy");
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import router from "./routes/routes.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(router);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
