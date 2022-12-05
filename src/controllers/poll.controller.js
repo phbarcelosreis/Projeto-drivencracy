e591a4609c64a0e5cd262b84372c6a6c1fb5717c
Pedro Reis <phbarcelosreis66@gmail.com>
2022-12-04 22:07:33 -0300

    feat: poll finished

--- src/controllers/poll.controller.js
+++ src/controllers/poll.controller.js
@@ -1,12 +1,44 @@
 import { poll } from "../database/db.js"
 
+function zero(n){
+    return n < 9 ? `0${n}` : `${n}`;
+}
+
+
+function formatDate(){
+    const data = new Date();
+    data.setDate(data.getDate() + 30);
+    const dia = zero(data.getDate());
+    const mes = zero(data.getMonth() + 1);
+    const ano = zero(data.getFullYear());
+    const hora = zero(data.getHours());
+    const minutos = zero(data.getMinutes());
+
+    return `${dia}-${mes}-${ano} ${hora}:${minutos}`
+}
+
 export async function pollPost(req, res) {
 
-    const survey = req.body;
+    const survey = req.body
+
+    const {title, expireAt} = req.body;
+    const data = formatDate();
 
     console.log(survey);
 
     try {
+
+        if(expireAt === ""){
+
+            const newObject = {
+                title,
+                expireAt: data
+            }
+
+            await poll.insertOne(newObject);
+            return res.sendStatus(201)
+
+        }
 
         await poll.insertOne(survey);
         res.sendStatus(201)
--- src/middlewares/pollValidate.middleware.js
+++ src/middlewares/pollValidate.middleware.js
@@ -1,26 +1,11 @@
 import {pollSchema} from '../models/model.js'
 /* import {choiceSchema} from '../models/pollModel.js' */
 
-function zero(n){
-    return n < 9 ? `0${n}` : `${n}`;
-}
 
-
-function formatDate(){
-    const data = new Date();
-    data.setDate(data.getDate() + 30);
-    const dia = zero(data.getDate());
-    const mes = zero(data.getMonth() + 1);
-    const ano = zero(data.getFullYear());
-
-    return `${dia}-${mes}-${ano}`
-}
 
 export async function pollPostValidate(req, res, next){
 
     const poll = req.body;
-    const newData = formatDate();
-    console.log(newData);
 
     const { error } = pollSchema.validate(poll, { abortEarly: false });
     if(error){
